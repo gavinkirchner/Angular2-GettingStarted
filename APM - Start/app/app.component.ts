1e7b53468043ab791d59b6f3ab439bdd87037503
Gavin Kirchner <gskirchner@gmail.com>
2017-07-20 11:20:30 -0500

    Got things working through Module 6

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ProductListComponent } from './products/product-list.component';
 
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <h1>{{pageTitle}}</h1>
+        <pm-productList></pm-productList>
     `
 })
-export class AppComponent { }
+export class AppComponent {
+    pageTitle: string = "My First Angular App!"
+}
